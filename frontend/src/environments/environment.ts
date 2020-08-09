2bfc89d96c23c2e3c53d6968c1539ed73707acd8
Abdelrahman Abdelnasser Gamal <55161963+kabaka121212@users.noreply.github.com>
2020-08-09 15:03:52 +0200

    Coffee Shop

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,14 @@
+/* @TODO replace with your variables
+ * ensure all variables on this page match your project
+ */
+
+export const environment = {
+  production: false,
+  apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
+  auth0: {
+    url: 'dev-wkpgv1un.us', // the auth0 domain prefix
+    audience: 'Coffee Shop', // the audience set for the auth0 app
+    clientId: '01KxJrgvBb2qML3Hro25097QPGw1YoXo', // the client id generated for the auth0 app
+    callbackURL: 'http://localhost:4200', // the base url of the running ionic application.
+  }
+};
